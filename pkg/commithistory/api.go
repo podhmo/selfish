b1fac624f924a67844eb1353a3434fc40804a598
podhmo <ababjam61+github@gmail.com>
2022-02-05 20:15:45 +0900

    refactor: remove unused comment

--- pkg/commithistory/api.go
+++ pkg/commithistory/api.go
@@ -4,12 +4,10 @@ import (
 	"github.com/podhmo/selfish/pkg/commithistory/history"
 )
 
-// API :
 type API struct {
 	*Config
 }
 
-// New :
 func New(name string, ops ...func(*API)) *API {
 	c := &API{Config: DefaultConfig()}
 	c.Config.Name = name
@@ -19,24 +17,12 @@ func New(name string, ops ...func(*API)) *API {
 	return c
 }
 
-// WithProfile :
 func WithProfile(profile string) func(*API) {
 	return func(c *API) {
 		c.Config.Profile = profile
 	}
 }
 
-// Load :
-func (c *API) Load(name string, ob interface{}) error {
-	return c.Config.Load(name, ob)
-}
-
-// Save :
-func (c *API) Save(name string, ob interface{}) error {
-	return c.Config.Save(name, ob)
-}
-
-// LoadCommit :
 func (c *API) LoadCommit(filename, alias string, ob history.Parsable) error {
 	dirpath, err := c.Dir(c.Name)
 	if err != nil {
@@ -46,7 +32,6 @@ func (c *API) LoadCommit(filename, alias string, ob history.Parsable) error {
 	return history.LoadFile(path, ob, alias)
 }
 
-// SaveCommit :
 func (c *API) SaveCommit(filename string, ob history.Unparsable) error {
 	dirpath, err := c.Dir(c.Name)
 	if err != nil {
@@ -56,7 +41,6 @@ func (c *API) SaveCommit(filename string, ob history.Unparsable) error {
 	return history.SaveFile(path, ob)
 }
 
-// IsNotFound :
 func (c *API) IsNotFound(err error) bool {
 	return history.IsNotFound(err)
 }
